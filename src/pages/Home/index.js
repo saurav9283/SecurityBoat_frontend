9f778226649f14ae25455caff76b1b583f39a95b
Saurav <sauravkumar00108@gmail.com>
2023-10-28 14:36:51 +0530

    changes in route

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,21 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './index.css'
 import Navbar from '../../components/NavBar'
 import Products from '../../components/Products'
 import AddProduct from '../../components/AddProduct'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 function Home({isChange,setIsChange}) {
+  const navigate = useNavigate();
     const [isUpdate,setIsUpdate]=useState(false);
     const [item,setItem]=useState({});
+
+    useEffect(()=>{
+      if(!localStorage.getItem("user"))
+      {
+          navigate("/login")
+      }
+    },[])
   return (
     <>
       <div className={isUpdate?`HomeDiv`:''}>
